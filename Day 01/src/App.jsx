b40f80ceb3de751647352d65a0c0045f7bda3dc6
strictlyManish <rajzmanish94@gmail.com>
2025-10-03 23:12:55 +0530

    relounch

--- Day 01/src/App.jsx
+++ Day 01/src/App.jsx
@@ -1,13 +1,9 @@
-import Navbar from "./Components/Navbar";
-import MainRoutes from "./Routes/MainRoutes";
+import React from 'react'
 
 function App() {
   return (
-    <div className="h-screen select-none w-screen px-10 overflow-x-hidden bg-[#010101] text-gray-100">
-      <Navbar />
-      <MainRoutes />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- Day 01/src/main.jsx
+++ Day 01/src/main.jsx
@@ -2,14 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter } from "react-router-dom";
-import Recipe from "./Context/Recipe.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <Recipe>
-    <BrowserRouter>
-      <App />
-      <Toaster position="top-right" reverseOrder={false} />
-    </BrowserRouter>
-  </Recipe>
+  <>
+    <App />
+    <Toaster position="top-right" reverseOrder={false} />
+  </>
 );
