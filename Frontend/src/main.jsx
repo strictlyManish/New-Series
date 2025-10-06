e72777dbfe7583a1fd83e955c78159f82875ac46
strictlyManish <rajzmanish94@gmail.com>
2025-10-06 21:40:40 +0530

    Adding or instlling react-redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,12 +3,16 @@ import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Store from "./store/Store.jsx";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <Provider stre={Store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
   </>
 );
